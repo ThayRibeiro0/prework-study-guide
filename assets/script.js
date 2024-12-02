114a6894034596916cc9ce9357f8e43015ad1cd5
Thays Ribeiro <thaysmoiaribeiro@gmail.com>
2024-12-02 09:41:02 -0800

    added javascript with topics worked until now

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,16 @@
-const topics = "HTML, CSS, Git, JavaScript";
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
 console.log(topics);
+/*
+if (topics === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topics === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topics === 'Git') {
+  console.log("Let's study Git!");
+} else if (topics === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
